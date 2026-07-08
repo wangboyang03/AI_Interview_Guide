30865540539eb149015b5b1b6579a3ce0423f331
wangboyang <18032391370@163.com>
2026-07-08 13:17:28 +0800

    build(app): 切换gradle国内镜像和maven仓库为阿里云镜像。

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,13 @@
 pluginManagement {
   repositories {
+    maven { url = uri("https://maven.aliyun.com/repository/google") }
+    maven { url = uri("https://maven.aliyun.com/repository/releases") }
+    maven { url = uri("https://maven.aliyun.com/repository/central") }
+    maven { url = uri("https://maven.aliyun.com/repository/public") }
+    maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+    maven { url = uri("https://maven.aliyun.com/repository/apache-snapshots") }
+    maven { url = uri("https://maven.aliyun.com/nexus/content/groups/public/") }
+    maven { url = uri("https://jitpack.io") }
     google {
       content {
         includeGroupByRegex("com\\.android.*")
@@ -17,6 +25,14 @@ plugins {
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
+    maven { url = uri("https://maven.aliyun.com/repository/google") }
+    maven { url = uri("https://maven.aliyun.com/repository/releases") }
+    maven { url = uri("https://maven.aliyun.com/repository/central") }
+    maven { url = uri("https://maven.aliyun.com/repository/public") }
+    maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+    maven { url = uri("https://maven.aliyun.com/repository/apache-snapshots") }
+    maven { url = uri("https://maven.aliyun.com/nexus/content/groups/public/") }
+    maven { url = uri("https://jitpack.io") }
     google()
     mavenCentral()
   }

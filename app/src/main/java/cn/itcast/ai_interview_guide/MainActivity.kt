7f93f1bf83d930e5059de551ecc22faf717bf4db
wangboyang <18032391370@163.com>
2026-07-08 13:07:30 +0800

    build(app): 初始化项目并下载相关依赖。

---
+++ app/src/main/java/cn/itcast/ai_interview_guide/MainActivity.kt
@@ -0,0 +1,47 @@
+package cn.itcast.ai_interview_guide
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import cn.itcast.ai_interview_guide.ui.theme.AI_Interview_GuideTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      AI_Interview_GuideTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+          Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding)
+          )
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+  Text(
+    text = "Hello $name!",
+    modifier = modifier
+  )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+  AI_Interview_GuideTheme {
+    Greeting("Android")
+  }
+}

b6d3aeda6e30ae336c8bbcb257219b47298555a7
wangboyang <18032391370@163.com>
2026-07-24 20:55:48 +0800

    feat(search): 将每一条记录插入存储修改为一整个列表形式保存，实现最新搜索词在第一位。

--- app/src/main/java/cn/itcast/ai_interview_guide/utils/DataStorePreferences.kt
+++ app/src/main/java/cn/itcast/ai_interview_guide/utils/DataStorePreferences.kt
@@ -19,41 +19,44 @@ class DataStorePreferences(context: Context) {
   private val dataStore = context.dataStore // 获取持久化存储实例
 
   /** 搜索关键词记录存储 **/
-   private fun createSearchKeywordKey(keyword: String) = stringPreferencesKey(Constants.SEARCH_RECORD_STORAGE_KEY + keyword)
+  // private fun createSearchKeywordKey(keyword: String) = stringPreferencesKey(Constants.SEARCH_RECORD_STORAGE_KEY + keyword)
+  private val SEARCH_RECORD_KEY = stringPreferencesKey(Constants.SEARCH_RECORD_STORAGE_KEY)
 
   // 添加搜索关键词记录
   suspend fun insertSearchKeyword(keyword: String) {
     // e.g. search_android = android
-    dataStore.edit { it[createSearchKeywordKey(keyword)] = keyword }
+    dataStore.edit {
+      // it[SEARCH_RECORD_KEY(keyword)] = keyword
+      val originalList = it[SEARCH_RECORD_KEY] ?: "[]" // ["keyword1", "keyword2", "keyword3"]
+      val createListByListOf = Json.decodeFromString<List<String>>(originalList).toMutableList() // 将JSON数据转换成集合
+      createListByListOf.remove(keyword) // 删除旧记录
+      createListByListOf.add(0, keyword) // 插入到最前面
+      it[SEARCH_RECORD_KEY] = Json.encodeToString(createListByListOf)
+    }
   }
 
   // 删除当前搜索关键词记录
   suspend fun deleteSearchKeyword(keyword: String) {
-    dataStore.edit { it.remove(createSearchKeywordKey(keyword)) }
+    dataStore.edit {
+      val getDataByJson = it[SEARCH_RECORD_KEY] ?: "[]"
+      val getDataList = Json.decodeFromString<MutableList<String>>(getDataByJson)
+      getDataList.remove(keyword)
+      it[SEARCH_RECORD_KEY] = Json.encodeToString(getDataList)
+    }
   }
 
   // 清空所有搜索记录
   suspend fun clearAllSearchKeyword() {
-    dataStore.edit { preferences ->
-      // 需要先找出首选项中所有包含搜索关键词的数据 放入新集合中
-      val includeSearchKeywordKeyByStorage = preferences.asMap().keys.filter { it.name.startsWith(Constants.SEARCH_RECORD_STORAGE_KEY) }
-      // 遍历并删除
-      includeSearchKeywordKeyByStorage.forEach {
-        preferences.remove(it)
-      }
+    dataStore.edit {
+      it.remove(SEARCH_RECORD_KEY)
     }
   }
 
   // 获取所有搜索关键词记录并返回
   suspend fun getAllSearchKeyword(): List<String> {
-    return dataStore.data.map { preferences ->
-      // 需要先找出首选项中所有包含搜索关键词的数据
-      preferences.asMap().filter {
-        it.key.name.startsWith(Constants.SEARCH_RECORD_STORAGE_KEY)
-      }.values.map {
-        // 把筛选到的数据存入一个新的集合
-        it.toString()
-      }
+    return dataStore.data.map {
+      val getDataByJson = it[SEARCH_RECORD_KEY] ?: "[]"
+      Json.decodeFromString<List<String>>(getDataByJson)
     }.first() // 取出冷流的第一个值
   }
 }
